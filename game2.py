e9a95ad87211019321f75017dd7530611e905779
Fernando <f.nlacerda5@gmail.com>
2023-11-27 13:41:49 -0300

    Initial

---
+++ game2.py
@@ -0,0 +1,141 @@
+import random
+
+from q import Q
+
+class Game:
+    def __init__(self, agent, agent2):
+        self.agentPair = (agent,agent2)
+        # initialize the game board
+        self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+
+    def checkForWin(self, key):
+        # check for player win on diagonals
+        a = [self.board[0][0], self.board[1][1], self.board[2][2]]
+        b = [self.board[0][2], self.board[1][1], self.board[2][0]]
+        if a.count(key) == 3 or b.count(key) == 3:
+            return True
+        # check for player win on rows/columns
+        for i in range(3):
+            col = [self.board[0][i], self.board[1][i], self.board[2][i]]
+            row = [self.board[i][0], self.board[i][1], self.board[i][2]]
+            if col.count(key) == 3 or row.count(key) == 3:
+                return True
+        return False
+
+    def checkForDraw(self):
+        draw = True
+        for row in self.board:
+            for elt in row:
+                if elt == '-':
+                    draw = False
+        return draw
+
+    def checkForEnd(self, key : str):
+        if self.checkForWin(key):
+            return 1000
+        elif self.checkForDraw():
+            return 0
+        return -1000
+
+    def training(self, humam):
+        playerNum = 1
+        opponentNum = 2
+        move = 1
+
+        # iterate until game is over
+        while True:
+
+            state = getStateKey(self.board)
+            if playerNum != humam:
+                action = self.agentPair[playerNum-1].getAction(state, playerNum)
+            else:
+                printBoard(state, move)
+                x = int(input())
+                y = int(input())
+                action = (x,y)
+    
+            move += 1
+
+            # execute oldAction, observe reward and state
+            self.board[action[0]][action[1]] = 'O' if playerNum == 1 else 'X'
+
+            check = self.checkForEnd('O' if playerNum == 1 else 'X')
+
+            newState = getStateKey(self.board)
+
+            if not check == -1000:
+                if check == 1000:
+                    myReward = check 
+                    opponentReward = -check
+                else:
+                    myReward = 100 if playerNum == 1 else 400
+                    opponentReward = 400 if playerNum == 1 else 100
+                break
+            else:
+                myReward = 0
+                opponentReward = 0
+            
+            
+            # update Q-values
+            self.agentPair[0].update(state, newState, action, myReward, playerNum)
+            self.agentPair[0].update(state, newState, action, opponentReward, opponentNum)
+
+            if type(self.agentPair[0]).__name__ != type(self.agentPair[1]).__name__:
+                self.agentPair[1].update(state, newState, action, myReward, playerNum)
+                self.agentPair[1].update(state, newState, action, opponentReward, opponentNum)
+
+            playerNum, opponentNum = opponentNum, playerNum
+
+        # Game over. Perform final update
+        if humam == 1 or humam == 2:
+            printBoard(state, move)
+
+        self.agentPair[0].update(newState, None, action, myReward, playerNum)
+        self.agentPair[0].update(state, newState, action, myReward, playerNum)
+
+        self.agentPair[0].update(newState, None, action, opponentReward, opponentNum)
+        self.agentPair[0].update(state, newState, action, opponentReward, opponentNum)
+        
+        if type(self.agentPair[0]).__name__ != type(self.agentPair[1]).__name__:
+            self.agentPair[1].update(newState, None, action, myReward, playerNum)
+            self.agentPair[1].update(state, newState, action, myReward, playerNum)
+
+            self.agentPair[1].update(newState, None, action, opponentReward, opponentNum)
+            self.agentPair[1].update(state, newState, action, opponentReward, opponentNum)
+
+        return myReward, playerNum
+    
+
+def printBoard(board, move = 0):
+    """
+    Prints the game board as text output to the terminal.
+
+    Parameters
+    ----------
+    board : list of lists
+        the current game board
+    """
+    print(f" Jogada {int( move/2 ) + 1} do jogador {move%2} : '{'O' if move%2 == 1 else 'X'}'")
+    print('    0   1   2\n')
+    for i in range(3):
+        print('%i   ' % i, end='')
+        for elt in range(3):
+            print('%s   ' % board[i * 3 + elt], end='')
+        print('\n')
+    print()
+
+def getStateKey(board):
+    """
+    Converts 2D list representing the board state into a string key
+    for that state. Keys are used for Q-value hashing.
+
+    Parameters
+    ----------
+    board : list of lists
+        the current game board
+    """
+    key = ''
+    for row in board:
+        for elt in row:
+            key += elt
+    return key
---
+++ minimax.py
@@ -0,0 +1,81 @@
+import random
+
+
+class Minimax:
+    def __init__(self, depth):
+        self.depth = depth
+        self.actions = []
+        for i in range(3):
+            for j in range(3):
+                self.actions.append((i,j))
+
+    def checkForWin(self, state : str, key : str):
+        # check for player win on diagonals
+        a = [state[0], state[4], state[8]]
+        b = [state[2], state[4], state[6]]
+        if a.count(key) == 3 or b.count(key) == 3:
+            return True
+        # check for player win on rows/columns
+        for i in range(3):
+            col = [state[i], state[3 + i], state[6 + i]]
+            row = [state[3 * i], state[3 * i + 1], state[3 * i + 2]]
+            if col.count(key) == 3 or row.count(key) == 3:
+                return True
+        return False
+
+    def getAction(self, state : str, playerNum : int):
+        if state.count("-") == 9:
+            return random.choice(self.actions)
+            #return (0,0)
+        value, action = self.minimax(state, self.depth, playerNum)
+        return action
+
+    def minimax(self, state : str, depth : int, playerNum : int, isMaximizing = True):
+
+        if state.count("-") == 0:
+            return (0.4 if playerNum == 1 else 0.1), None
+
+        if depth == 0:
+            return 0, None
+
+        possibleActions = [a for a in self.actions if state[a[0]*3 + a[1]] == '-']
+
+        if isMaximizing:
+            max = -float("inf")
+
+            for a in possibleActions:
+                l = list ( state )
+                l[a[0]*3 + a[1]] = "O" if playerNum == 1 else "X"
+                newState = ''.join( l )
+                if self.checkForWin(newState, "O" if playerNum == 1 else "X"): # eu 
+                    value = 1
+                elif self.checkForWin(newState, "X" if playerNum == 1 else "O"): # ele
+                    value = 0
+                else:
+                    value, _ = self.minimax(newState, depth-1, 2 if playerNum == 1 else 1, not isMaximizing)
+
+                if value > max:
+                    max = value
+                    action = a
+        else:
+            max = float("inf")
+
+            for a in possibleActions:
+                l = list ( state )
+                l[a[0]*3 + a[1]] = "O" if playerNum == 1 else "X"
+                newState = ''.join( l )
+
+                if self.checkForWin(newState, "O" if playerNum == 1 else "X"): # eu
+                    value = 0
+                elif self.checkForWin(newState, "X" if playerNum == 1 else "O"): # ele
+                    value = 1
+                else:
+                    value, _ = self.minimax(newState, depth-1, 2 if playerNum == 1 else 1, not isMaximizing)
+                if value < max:
+                    max = value
+                    action = a
+
+        return max, action
+
+    def update(self, oldState : str, newState : str, action : tuple[int, int], reward : int, playerNum : int):
+        pass
